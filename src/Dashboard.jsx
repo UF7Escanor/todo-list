822650a66a9bd28cd2d9d03fac58641314945b1e
rahul <nameisrahulsenthakuri@gmail.com>
2025-06-09 11:16:26 +0545

    completed

---
+++ src/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <div>
+        <h1>To do list</h1>
+        <input placeholder="ASK YOUR TASK"></input>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
